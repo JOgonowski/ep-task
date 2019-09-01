796c5259b08704a2f558b5f362cf612a0af74877
Jacek Ogonowski <jacek.ogonowski@considonet.com>
2019-09-01 21:50:10 +0200

    Task done + config update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ep-task
+
+Run "serve" script to build assets and serve the page locally under the 8080 port.
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require("postcss-normalize")({ forceImport: true }),
-    require("postcss-preset-env")({ stage: 0 }),
+    require("postcss-preset-env")({ stage: 0, autoprefixer: { grid: true } }),
     require("postcss-flexbugs-fixes")()
   ]
 };
